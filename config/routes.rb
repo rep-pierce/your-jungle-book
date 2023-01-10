529b5a64d8a31735596da76a9c19a7f22cdcdb9e
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-01-10 11:35:32 -0700

    fixed stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/hello', to: 'application#hello_world'
+
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
