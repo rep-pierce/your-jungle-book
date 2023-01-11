4b5adae5085d4fcdd9c68004bf151e0894d8cdb5
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-01-10 17:28:25 -0700

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,14 @@ class PostsController < ApplicationController
         posts = Post.all
         render json: posts
     end
+
+    def create
+        post = Post.create!(post_params)
+    end
+
+    private
+
+    def post_params
+        params.permit(:user_id, :title, :image, :post_body)
+    end
 end
