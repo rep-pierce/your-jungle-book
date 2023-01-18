805efab1aa2ab0718813dd2fcc24a3dbea3c5ea8
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-01-18 11:27:06 -0700

    made loading page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -4,6 +4,9 @@ import { Context } from '../contexts/Context'
 
 function HomePage() {
   const {posts} = useContext(Context)
+  if (!posts){
+    return <div>Loading...</div>
+  }
   // takes the list of posts and renders them on the page.
   function renderPosts(){
     return posts.map( post => <PostCard key={post.id} post={post} />)
